3a9e91a8f9d9e38f18c9c835670c8a4792029709
Michael Leo <yhzemail61010@aliyun.com>
2022-08-17 16:46:26 +0800

    init

--- app/src/test/kotlin/com/leovp/androidtemplate/ExampleUnitTest.kt
+++ app/src/test/kotlin/com/leovp/androidtemplate/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.leovp.androidtemplate
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
