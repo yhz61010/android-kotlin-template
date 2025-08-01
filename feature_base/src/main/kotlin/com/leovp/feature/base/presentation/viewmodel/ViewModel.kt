ca0e990828c13dfaea8064b9dce2eaf3fe1d4a36
Michael Leo <yhzemail61010@aliyun.com>
2025-08-01 09:22:58 +0800

    Add more method in ViewModel.kt

--- feature_base/src/main/kotlin/com/leovp/feature/base/presentation/viewmodel/ViewModel.kt
+++ feature_base/src/main/kotlin/com/leovp/feature/base/presentation/viewmodel/ViewModel.kt
@@ -16,8 +16,13 @@
 
 package com.leovp.feature.base.presentation.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 
 /**
  * Returns a [ViewModelProvider.Factory] which will return the result of [create] when it's
@@ -43,5 +48,134 @@ private class SimpleFactory<VM : ViewModel>(
             return vm as T
         }
         throw IllegalArgumentException("Can not create ViewModel for class: $modelClass")
+    }
+}
+
+/**
+ * Usage:
+ * In your own view model:
+ * ```
+ * class HomeViewModel(
+ *     private val savedStateHandle: SavedStateHandle,
+ *     private val useCase: UseCase
+ * ) :
+ *     BaseViewModel<UiState, BaseAction<UiState>>(
+ *         useCase = useCase,
+ *         initialState = initialState
+ *     ) {
+ *     companion object {
+ *         val USE_CASE_KEY = object : CreationExtras.Key<UseCase> {}
+ *
+ *         val Factory: ViewModelProvider.Factory = viewModelFactoryWithSavedState(
+ *             key1 = USE_CASE_KEY
+ *         ) { savedStateHandle, useCase ->
+ *             HomeViewModel(useCase = useCase, savedStateHandle = savedStateHandle)
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * In your navigation graph or something others:
+ * ```
+ * composable(route = Screen.Home.route) { backStackEntry ->
+ *     val ctx = LocalContext.current
+ *     AppTheme {
+ *         val homeVM: HomeViewModel = viewModel(
+ *             viewModelStoreOwner = backStackEntry,
+ *             factory = HomeViewModel.Factory,
+ *             extras = backStackEntry.defaultViewModelCreationExtras + MutableCreationExtras().apply {
+ *                 set(USE_CASE_KEY, UseCase(RepositoryImpl()))
+ *             },
+ *         )
+ *     }
+ * }
+ * ```
+ */
+inline fun <reified VM : ViewModel, reified Param> viewModelFactoryWithSavedState(
+    key: CreationExtras.Key<Param>,
+    crossinline create: (savedStateHandle: SavedStateHandle, param: Param) -> VM
+): ViewModelProvider.Factory {
+    return viewModelFactory {
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            val param = this[key] ?: error("Missing ViewModel parameter for key: $key")
+            create(savedStateHandle, param)
+        }
+    }
+}
+
+/**
+ * Usage:
+ * In your own view model:
+ * ```
+ * class YourViewModel(
+ *     private val savedStateHandle: SavedStateHandle,
+ *     private val useCase: YourUseCase,
+ *     initialState: UiState = Loading,
+ * ) :
+ *     BaseViewModel<UiState, BaseAction<UiState>>(
+ *         useCase = useCase,
+ *         initialState = initialState
+ *     ) {
+ *     companion object {
+ *         val USE_CASE_KEY = object : CreationExtras.Key<YourUseCase> {}
+ *         val TYPE = object : CreationExtras.Key<YourTypeModel> {}
+ *
+ *         val Factory: ViewModelProvider.Factory = viewModelFactoryWithSavedState(
+ *             key1 = USE_CASE_KEY,
+ *             key2 = TYPE,
+ *         ) { savedStateHandle, useCase, type ->
+ *             savedStateHandle["type"] = type
+ *             YourViewModel(useCase = useCase, savedStateHandle = savedStateHandle)
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * In your navigation graph or something others:
+ * ```
+ * composable(route = Screen.Info.route) { backStackEntry ->
+ *     val ctx = LocalContext.current
+ *     AppTheme {
+ *         val homeVM: YourViewModel = viewModel(
+ *             viewModelStoreOwner = backStackEntry,
+ *             factory = YourViewModel.Factory,
+ *             extras = backStackEntry.defaultViewModelCreationExtras + MutableCreationExtras().apply {
+ *                 set(USE_CASE_KEY, YourUseCase(YourRepositoryImpl()))
+ *             },
+ *         )
+ *     }
+ * }
+ * ```
+ */
+inline fun <reified VM : ViewModel, reified Param1, reified Param2> viewModelFactoryWithSavedState(
+    key1: CreationExtras.Key<Param1>,
+    key2: CreationExtras.Key<Param2>,
+    crossinline create: (savedStateHandle: SavedStateHandle, param1: Param1, param2: Param2) -> VM
+): ViewModelProvider.Factory {
+    return viewModelFactory {
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            val param1 = this[key1] ?: error("Missing ViewModel parameter for key1: $key1")
+            val param2 = this[key2] ?: error("Missing ViewModel parameter for key2: $key2")
+            create(savedStateHandle, param1, param2)
+        }
+    }
+}
+
+inline fun <reified VM : ViewModel, reified Param1, reified Param2, reified Param3> viewModelFactoryWithSavedState(
+    key1: CreationExtras.Key<Param1>,
+    key2: CreationExtras.Key<Param2>,
+    key3: CreationExtras.Key<Param3>,
+    crossinline create: (savedStateHandle: SavedStateHandle, param1: Param1, param2: Param2, param3: Param3) -> VM
+): ViewModelProvider.Factory {
+    return viewModelFactory {
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            val param1 = this[key1] ?: error("Missing ViewModel parameter for key1: $key1")
+            val param2 = this[key2] ?: error("Missing ViewModel parameter for key2: $key2")
+            val param3 = this[key3] ?: error("Missing ViewModel parameter for key3: $key3")
+            create(savedStateHandle, param1, param2, param3)
+        }
     }
 }
